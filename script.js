358ab492abb14bdb435b1789848dd30d2d562623
Cauã Arthur <cauaarthurdossantos@gmail.com>
2024-10-21 21:19:40 -0300

    to indo

---
+++ script.js
@@ -0,0 +1,51 @@
+let res = [document.getElementById('visor')]
+let num = []
+
+function n1() {
+    res.innerHTML += 1
+}
+
+function n2() {
+    res.innerHTML +=2
+}
+
+function n3() {
+    res.innerHTML +=3
+}
+
+function n4() {
+    res.innerHTML +=4
+}
+
+function n5() {
+    res.innerHTML +=5
+}
+
+function n6() {
+    res.innerHTML +=6
+}
+
+function n7() {
+    res.innerHTML +=7
+}
+
+function n8() {
+    res.innerHTML +=8
+}
+
+function n9() {
+    res.innerHTML +=9
+}
+
+function n0() {
+    res.innerHTML +=0
+}
+
+function soma() {
+    num = res
+    res.innerHTML = ''
+}
+
+function resultado() {
+    res.innerHTML = num
+}
