0d9021eb2661de9430d52d0208f147ba00259860
Cauã Arthur <cauaarthurdossantos@gmail.com>
2024-10-26 18:01:55 -0300

    calculo com array e redução das funções de botões

--- script.js
+++ script.js
@@ -1,52 +1,19 @@
-var res = document.getElementById('visor')
-var total = 0
+var res = document.getElementById('resposta')
+var val = document.getElementById('visor')
+let total = []
 
-function n1() {
-    res.value += 1
-}
-
-function n2() {
-    res.value += 2
-}
-
-function n3() {
-    res.value += 3
-}
-
-function n4() {
-    res.value += 4
-}
-
-function n5() {
-    res.value += 5
-}
-
-function n6() {
-    res.value += 6
-}
-
-function n7() {
-    res.value += 7
-}
-
-function n8() {
-    res.value += 8
-}
-
-function n9() {
-    res.value += 9
-
-}
-
-function n0() {
-    res.value += 0
+function botarnumero(num) {
+    val.value += num
 }
 
 function soma() {
-    if (res.value == '') {
-        window.alert('nenhum numero digitado')
+    if (total[0] === undefined) {
+        total[0] = Number(val.value)
     } else {
-        total += Number(res.value)
-        res.value = total
+        total[1] = Number(val.value)
+        var soma = total[0] + total[1]
+        res.innerHTML = soma
+        total = []
     }
+    val.value = ''
 }
--- teste/script.js
+++ teste/script.js
@@ -1,25 +1,15 @@
 var res = document.getElementById('resultado')
 var val = document.getElementById('valor')
-var total = 0
+let total = []
 
 function soma() {
-    if (val.value == '') {
-        window.alert('digita')
+    if (total[0] === undefined) {
+        total[0] = Number(val.value)
     } else {
-        total += Number(val.value)
-        val.value = total
+        total[1] = Number(val.value)
+        var soma = total[0] + total[1]
+        res.innerHTML = soma
+        total = []
     }
-}
-
-function resultado() {
-    var vl1 = document.getElementById('valor')
-    var vl2 = document.getElementById('valor')
-    if (vl1 == 0) {
-        vl1.value = val
-    } else {
-        vl2.value = val
-    }
-    var soma = vl1 + vl2
-    res.innerText = soma
-
+    val.value = ''
 }
