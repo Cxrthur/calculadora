2e8b781478efc0e481debb19e5a5aea7e1accf94
Cauã Arthur <cauaarthurdossantos@gmail.com>
2024-10-29 10:03:45 -0300

    FALTA TODO O RESTO

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 var res = document.getElementById('resposta')
+var result = document.getElementById('resultado')
 var val = document.getElementById('visor')
 let total = []
+let resultsoma = null
 
 function botarnumero(num) {
     val.value += num
+}
+
+function iguala() {
+    if (total.length === 2) {
+        var soma = total[0] + total[1]
+        result.innerHTML = soma
+        result.style.display = 'block'
+        val.style.display = 'none'
+        total = []
+    }
+    val.value = ''
 }
 
 function soma() {
@@ -11,9 +24,6 @@ function soma() {
         total[0] = Number(val.value)
     } else {
         total[1] = Number(val.value)
-        var soma = total[0] + total[1]
-        res.innerHTML = soma
-        total = []
     }
     val.value = ''
 }
